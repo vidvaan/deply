9fa9b0d7735b023df94633d0e9be986c367e93bb
goddub <goddub@PC1GFHB6.one.ads.bms.com>
2023-06-05 07:13:34 +0530

    itit

---
+++ 005HelloWorld/src/main/java/com/createiq/TestController.java
@@ -0,0 +1,12 @@
+package com.createiq;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/hello")
+	public String name() {
+		return "Balaji";
+	}
+}
